83ce990cc1616edc3ed0cfba4b53c9faf8e3032e
Bassel <basselkanso82@gmail.com>
2020-06-03 21:51:56 +0300

    Feature: Get contacts from API
    -add useEffect hook to fetch contacts from API
    -use environment variable for API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Home from './pages/home/Home';
 import Edit from './pages/edit/Edit';
 import View from './pages/view/View';
@@ -6,28 +6,26 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-    useHistory,
   } from "react-router-dom";
 
-const data = [
-{
-    id:1,
-    email:"bassel@codi.tech",
-    last_name:"Kanso",
-    first_name:"Bassel",
-    phone_number:"899016756"
-},
-{
-    id:2,
-    email:"bassel@gmail.com",
-    last_name:"Kanso",
-    first_name:"Bassel",
-    phone_number:"76307825"
-},
-]
 
 const App = () => {
-    const [contacts,setContacts] = useState(data)
+    const [contacts,setContacts] = useState([])
+
+    useEffect(() => {
+        async function getContacts () {
+            try{
+                const response = await fetch(
+                `${process.env.REACT_APP_API}/contacts`);
+                const result = await response.json();
+                setContacts(result);
+            }catch(error){
+                console.log(error);
+            }
+        }
+
+        getContacts();
+      });
 
     const deleteContact = (id) => {
         const newContacts = contacts.filter((contact)=>contact.id !== id);
